8752c84c6ae10f7f07bcf2e3c2e2e8b3643e9fca
bensta <9579702+bensta@users.noreply.github.com>
2020-12-16 18:04:48 +0100

    add simple hello world dag

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.contrib.hooks.gcs_hook import GoogleCloudStorageHook
+from airflow.hooks.postgres_hook import PostgresHook
+
+from datetime import datetime, timedelta
+import logging
+import os
+
+def train_model():
+    print("hello world")
+
+with DAG('training_pipeline', start_date=datetime(2016, 1, 1)) as dag:
+    t1 = PythonOperator( 
+        task_id='train',
+        python_callable=train_model,
+        dag=dag)
